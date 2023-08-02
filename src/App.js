f8cf3dcd8bfa8bcacb2f30ebaf018055b4dec14f
Shirley Yip <abc1c1sam@gmail.com>
2023-08-02 15:21:14 +0800

    icon

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./App.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import About from "./pages/About";
+// import About from "./pages/About";
 import Products from "./pages/Products";
-import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
+// import Home from "./pages/Home";
+import Navbar from "./Navbar";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/" />
+          {/* <Route path="/about" element={<About />} /> */}
           <Route path="/products" element={<Products />} />
         </Routes>
       </BrowserRouter>
